8ce98ee931f191beb7c2ad33fe784613395ca31a
David <Davidaguayo1379@gmail.com>
2023-09-05 04:04:13 -0700

    Initial Commit

---
+++ Exercise2_Aguayo_David/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exercise2_Aguayo_David
+//
+//  Created by user235932 on 9/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
